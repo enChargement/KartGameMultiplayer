89fcdada0b7a11c2a42d857d3c5b4fe59904504c
enChargement <thibaudking@gmail.com>
2023-05-07 00:11:59 +0200

    drift physic finished

--- Assets/Scripts/CarManager.cs
+++ Assets/Scripts/CarManager.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.VFX;
@@ -9,11 +10,14 @@ public class CarManager : MonoBehaviour
 
     private float _steer = 0f;
     private float _acceleration = 0f;
-    private bool _drift = false;
+    private bool _canDrift = false;
+    public bool _isDrifting = false;
+    private float _driftTimer = 0f;
     private bool _grounded = true;
 
     private Vector3 _localVelocity;
 
+    [Header("Vehicule Parameters")]
     public float speed = 100f;
     public float turn = 5f;
     public float gravityMultiplier = 10f;
@@ -21,18 +25,32 @@ public class CarManager : MonoBehaviour
     public float RealignementForce = 10f;
     public float visualSpeed = 2f;
     public float additionnalSpeed = 0f;
+    public float DriftImpulseForce = 30f;
 
+    [Space(10)]
+    public Transform GroundedPoint;
+
+    [Header("Wheels Transform")]
     public Transform FL_turn;
     public Transform FR_turn;
 
+    [Space(10)]
     public Transform FL;
     public Transform FR;
     public Transform BL;
     public Transform BR;
 
-    public int ParticleIndex = 0;
+    [Header("Drift Parameters")]
+    [Range(0f,5f)]
+    public float InversionAxisVelocity = 1f;
+    [Range(0f, 1f)]
+    public float ConstantTurnDrift = 0.6f;
+    [Range(0f, 4f)]
+    public float DriftOverSteerDivider = 2f;
 
-    public float TempInversionVelocity = 1f;
+    [Header("Particles Parameters")]
+    private int _sparklesIndex = 0;
+    public int ParticleIndex { get { return _sparklesIndex; } set { _sparklesIndex = value; } }
 
     public List<ParticleSystem> LeftSparkles = new List<ParticleSystem>();
 
@@ -40,11 +58,6 @@ public class CarManager : MonoBehaviour
 
 
     public List<TrailRenderer> TrailDrifts = new List<TrailRenderer>();
-
-
-    public Transform GroundedPoint;
-
-
 
     void Start()
     {
@@ -70,15 +83,23 @@ public class CarManager : MonoBehaviour
         Debug.Log("drift velocity : "+ _localVelocity);
 
         //car visuals
+    }
+
+    private void Update()
+    {
         SteeringWheel();
         TurnWheel();
+        CheckSparklesDrift();
     }
+
+
+
     #region Before Physic
     private void GetInputs()
     {
         _steer = Input.GetAxis("Horizontal");
         _acceleration = Input.GetAxis("Vertical");
-        _drift = Input.GetKey(KeyCode.Space);
+        _canDrift = Input.GetKey(KeyCode.Space);
     }
     private void IsGrounded()
     {
@@ -95,60 +116,7 @@ public class CarManager : MonoBehaviour
         if (_acceleration > 0f && _grounded)
             rb.AddRelativeForce(Vector3.forward * (speed+additionnalSpeed));
         else if (_acceleration < 0f && _grounded)
-            rb.AddRelativeForce(Vector3.forward * (-speed-additionnalSpeed)); 
-    }
-
-    private void Drifting()
-    {
-        /*if(rb.velocity.magnitude < 6.2f)
-        {
-            _drift = false;
-        }*/
-        if (_drift && _grounded)
-        {
-            float steeringSens = _steer != 0 ? _steer : -_localVelocity.x;
-
-            float yRot = rb.transform.rotation.eulerAngles.y + (SteeringForce * 0.6f + Mathf.Abs(_steer) * SteeringForce) * (steeringSens > 0 ? 1 : -1) * (_localVelocity.z > 0 ? 1 : -1) * Time.fixedDeltaTime;
-
-            rb.rotation = Quaternion.Euler(new Vector3(rb.transform.rotation.eulerAngles.x, yRot, rb.transform.rotation.eulerAngles.z));
-            if (_localVelocity.x < 0)
-            {
-                _localVelocity.x = _localVelocity.x - TempInversionVelocity * Time.fixedDeltaTime;
-            }
-            else
-            {
-                _localVelocity.x = _localVelocity.x + TempInversionVelocity * Time.fixedDeltaTime;
-            }
-            _localVelocity.z = _localVelocity.z + TempInversionVelocity * Time.fixedDeltaTime;
-            rb.velocity = transform.TransformDirection(_localVelocity);
-            for (int i = 0; i < 3; i++)
-            {
-                ParticleSystem psRight = RightSparkles[i];
-                ParticleSystem psLeft = LeftSparkles[i];
-                if (i == ParticleIndex)
-                {
-                    psRight.Play();
-                    psLeft.Play();
-                    Debug.Log("rank : " + i);
-                }
-                else
-                {
-                    psRight.Stop();
-                    psLeft.Stop();
-                }
-            }
-            TrailDrifts.ForEach(trail => trail.emitting = true);
-            
-        }
-
-        else
-        {
-            _localVelocity.x = 0;
-            rb.velocity = transform.TransformDirection(_localVelocity);
-            LeftSparkles.ForEach(item => item.Stop());
-            RightSparkles.ForEach(item => item.Stop());
-            TrailDrifts.ForEach(trail => trail.emitting = false);
-        }
+            rb.AddRelativeForce(Vector3.forward * (-(speed/1.5f)-additionnalSpeed)); 
     }
 
     private void TurnForce()
@@ -158,9 +126,93 @@ public class CarManager : MonoBehaviour
         if (Mathf.Abs(_localVelocity.z) > 0.1f)
         {
             float yRot = rb.transform.rotation.eulerAngles.y + _steer * SteeringForce * (_localVelocity.z > 0 ? 1 : -1) * (4-Mathf.Abs(_acceleration))/4 * Time.fixedDeltaTime;
-            float lerpRot = Mathf.LerpAngle(rb.transform.rotation.eulerAngles.y, yRot, 10);
-            rb.rotation = Quaternion.Euler(new Vector3(rb.transform.rotation.eulerAngles.x, lerpRot, rb.transform.rotation.eulerAngles.z));
+            float lerpRot = Mathf.LerpAngle(rb.transform.rotation.eulerAngles.y, yRot, 1);
+            rb.rotation = Quaternion.Euler(new Vector3(rb.transform.rotation.eulerAngles.x, yRot, rb.transform.rotation.eulerAngles.z));
         }
+    }
+
+    private void Drifting()
+    {
+        if(rb.velocity.magnitude < 5f || _localVelocity.z < 0 || (Mathf.Abs(_steer) < .65f && !_isDrifting))
+        {
+            _canDrift = false;
+        }
+        if (_canDrift && _grounded)
+        {
+            _driftTimer += Time.fixedDeltaTime;
+
+            DriftTurn();
+
+            InversionVelocity();
+
+            if (_isDrifting) { return; }
+
+            _isDrifting = true;
+            PlayDriftParticles();
+        }
+        else
+        {
+            _localVelocity.x /= 5 ;
+            rb.velocity = transform.TransformDirection(_localVelocity);
+
+            if (_isDrifting)
+            {
+                _isDrifting = false;
+                DriftImpulse();
+                _sparklesIndex = -1;
+                StopDriftParticles();
+            }
+
+        }
+    }
+
+    /// <summary>
+    /// Defines the rotation of the car during the drift
+    /// </summary>
+    private void DriftTurn()
+    {
+        float turnDirection = (-_localVelocity.x > 0 ? 1 : -1);
+
+        float constantTurnInDrift = SteeringForce * ConstantTurnDrift * turnDirection;
+
+        float additionalTurn = (_steer + 1 * turnDirection) / DriftOverSteerDivider * SteeringForce;
+
+
+        float yRot = rb.transform.rotation.eulerAngles.y + (constantTurnInDrift + additionalTurn) * Time.fixedDeltaTime;
+        float lerpRot = Mathf.LerpAngle(rb.transform.rotation.eulerAngles.y, yRot, .8f);
+
+        rb.rotation = Quaternion.Euler(new Vector3(rb.transform.rotation.eulerAngles.x, lerpRot, rb.transform.rotation.eulerAngles.z));
+    }
+
+    /// <summary>
+    /// invere the amount of velocity between x and z axis to simulate drifting
+    /// </summary>
+    private void InversionVelocity()
+    {
+        if (_localVelocity.x < 0)
+        {
+            _localVelocity.x = _localVelocity.x - InversionAxisVelocity * Time.fixedDeltaTime;
+        }
+        else
+        {
+            _localVelocity.x = _localVelocity.x + InversionAxisVelocity * Time.fixedDeltaTime;
+        }
+        _localVelocity.z = _localVelocity.z + InversionAxisVelocity / 2 * Time.fixedDeltaTime;
+        rb.velocity = transform.TransformDirection(_localVelocity);
+    }
+
+    /// <summary>
+    /// add an impulse to the car in proportion to the drift length
+    /// </summary>
+    private void DriftImpulse()
+    {
+        if(_sparklesIndex == -1)
+        {
+            return;
+        }
+        rb.AddForce(transform.forward * (DriftImpulseForce + _sparklesIndex * DriftImpulseForce),ForceMode.Impulse);
+        _driftTimer = 0f;
+        
     }
 
     private void AddGravity()
@@ -185,7 +237,7 @@ public class CarManager : MonoBehaviour
     private void SteeringWheel()
     {
         var leftLocalEuler = FL_turn.localEulerAngles;
-        leftLocalEuler.y = _steer * 30f;
+        leftLocalEuler.y = _steer * 40f;
         FL_turn.localEulerAngles = leftLocalEuler;
         FR_turn.localEulerAngles = leftLocalEuler;
     }
@@ -197,6 +249,58 @@ public class CarManager : MonoBehaviour
         FR.transform.Rotate(Vector3.right, _localVelocity.z * visualSpeed);
         BL.transform.Rotate(Vector3.right, _localVelocity.z * visualSpeed);
         BR.transform.Rotate(Vector3.right, _localVelocity.z * visualSpeed);
+    }
+
+    private void PlayDriftParticles()
+    {
+        for (int i = 0; i < 3; i++)
+        {
+            ParticleSystem psRight = RightSparkles[i];
+            ParticleSystem psLeft = LeftSparkles[i];
+            if (i == ParticleIndex)
+            {
+                if (!psRight.isPlaying)
+                    psRight.Play();
+                if (!psLeft.isPlaying)
+                    psLeft.Play();
+                Debug.Log("rank : " + i);
+            }
+            else
+            {
+                if (psLeft.isPlaying)
+                    psLeft.Stop();
+                if (psRight.isPlaying)
+                    psRight.Stop();
+            }
+        }
+        TrailDrifts.ForEach(trail => trail.emitting = true);
+    }
+
+    private void SetSparklesIndex(int index)
+    {
+        if(index == _sparklesIndex)
+        {
+            return;
+        }
+        _sparklesIndex = index;
+        PlayDriftParticles();
+    }
+
+    private void StopDriftParticles()
+    {
+        LeftSparkles.ForEach(item => item.Stop());
+        RightSparkles.ForEach(item => item.Stop());
+        TrailDrifts.ForEach(trail => trail.emitting = false);
+    }
+
+    private void CheckSparklesDrift()
+    {
+        if (!_isDrifting) return;
+
+        if (_driftTimer < 1.5f) { SetSparklesIndex(-1); }
+        else if (_driftTimer < 2.5f) { SetSparklesIndex(0); }
+        else if (_driftTimer < 4f) { SetSparklesIndex(1); }
+        else if (_driftTimer >= 4) { SetSparklesIndex(2); }
     }
 
     #endregion
